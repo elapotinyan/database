ca6f190aa73bd623e547d9127207a31d09984eb9
Эла <elapotinyan@icloud.com>
2024-12-30 23:37:30 +0400

    добавила маленькие изменения

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from schemas import StateCreate, StateSchema, NationalityCreate, NationalitySche
 
 app = FastAPI()
 
-# Функция для получения сессии базы данных
 def get_db():
     db = SessionLocal()
     try:
@@ -13,7 +12,6 @@ def get_db():
     finally:
         db.close()
 
-# Create - создание новой записи для Государства
 @app.post("/states/", response_model=StateSchema)
 def create_state(state: StateCreate, db: Session = Depends(get_db)):
     db_state = State(name=state.name, capital=state.capital, government_type=state.government_type)
@@ -22,13 +20,13 @@ def create_state(state: StateCreate, db: Session = Depends(get_db)):
     db.refresh(db_state)
     return db_state
 
-# Read - получение списка всех Государств
+
 @app.get("/states/", response_model=list[StateSchema])
 def get_states(db: Session = Depends(get_db), skip: int = 0, limit: int = 10):
     states = db.query(State).offset(skip).limit(limit).all()
     return states
 
-# Read - получение записи Государства по ID
+
 @app.get("/states/{state_id}", response_model=StateSchema)
 def get_state(state_id: int, db: Session = Depends(get_db)):
     state = db.query(State).filter(State.id == state_id).first()
@@ -50,7 +48,6 @@ def update_state(state_id: int, state: StateCreate, db: Session = Depends(get_db
     db.refresh(db_state)
     return db_state
 
-# Delete - удаление записи Государства
 @app.delete("/states/{state_id}", response_model=StateSchema)
 def delete_state(state_id: int, db: Session = Depends(get_db)):
     db_state = db.query(State).filter(State.id == state_id).first()
@@ -60,7 +57,7 @@ def delete_state(state_id: int, db: Session = Depends(get_db)):
     db.delete(db_state)
     db.commit()
     return db_state
-# Create - создание новой записи для Национальности
+
 @app.post("/nationalities/", response_model=NationalitySchema)
 def create_nationality(nationality: NationalityCreate, db: Session = Depends(get_db)):
     db_nationality = Nationality(name=nationality.name, language=nationality.language, total_population=nationality.total_population)
@@ -69,13 +66,12 @@ def create_nationality(nationality: NationalityCreate, db: Session = Depends(get
     db.refresh(db_nationality)
     return db_nationality
 
-# Read - получение списка всех Национальностей
+
 @app.get("/nationalities/", response_model=list[NationalitySchema])
 def get_nationalities(db: Session = Depends(get_db), skip: int = 0, limit: int = 10):
     nationalities = db.query(Nationality).offset(skip).limit(limit).all()
     return nationalities
 
-# Read - получение записи Национальности по ID
 @app.get("/nationalities/{nationality_id}", response_model=NationalitySchema)
 def get_nationality(nationality_id: int, db: Session = Depends(get_db)):
     nationality = db.query(Nationality).filter(Nationality.id == nationality_id).first()
@@ -106,15 +102,15 @@ def delete_nationality(nationality_id: int, db: Session = Depends(get_db)):
     db.delete(db_nationality)
     db.commit()
     return db_nationality
-# Create - создание новой записи для Населения
+
 @app.post("/populations/", response_model=PopulationSchema)
 def create_population(population: PopulationCreate, db: Session = Depends(get_db)):
-    # Проверяем, существует ли state с указанным state_id
+   
     db_state = db.query(State).filter(State.id == population.state_id).first()
     if db_state is None:
         raise HTTPException(status_code=404, detail="State not found")
 
-    # Проверяем, существует ли nationality с указанным nationality_id
+   
     db_nationality = db.query(Nationality).filter(Nationality.id == population.nationality_id).first()
     if db_nationality is None:
         raise HTTPException(status_code=404, detail="Nationality not found")
@@ -132,19 +128,18 @@ def create_population(population: PopulationCreate, db: Session = Depends(get_db
         db.commit()
         db.refresh(db_population)
     except Exception as e:
-        db.rollback()  # Откатываем изменения в случае ошибки
+        db.rollback()  
         raise HTTPException(status_code=500, detail=f"Error creating population: {str(e)}")
 
     return db_population
 
 
-# Read - получение списка всех записей Населения
+
 @app.get("/populations/", response_model=list[PopulationSchema])
 def get_populations(db: Session = Depends(get_db), skip: int = 0, limit: int = 10):
     populations = db.query(Population).offset(skip).limit(limit).all()
     return populations
 
-# Read - получение записи Населения по ID
 @app.get("//{population_id}", response_model=PopulationSchema)
 def get_population(population_id: int, db: Session = Depends(get_db)):
     population = db.query(Population).filter(Population.id == population_id).first()
