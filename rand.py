baf4c6c6a498a7065d265d7b2ac724ed8c9b1854
Эла <elapotinyan@icloud.com>
2024-12-31 01:42:15 +0400

    добавила файл для заполнения данными для населения и генерации 50 значений

--- rand.py
+++ rand.py
@@ -35,3 +35,28 @@ def create_nationality():
         print(f"Не удалось создать национальность: {nationality_data['name']}")
         return None
     
+def create_population(state_id, nationality_id):
+    population_data = {
+        "state_id": state_id,
+        "nationality_id": nationality_id,
+        "male_population": random.randint(10000, 50000000),
+        "female_population": random.randint(10000, 50000000),
+        "total_population": random.randint(20000, 100000000)
+    }
+    response = requests.post(f"{BASE_URL}/populations/", json=population_data)
+    if response.status_code == 200:
+        print(f"Население создано для государства {state_id} и национальности {nationality_id}")
+    else:
+        print(f"Не удалось создать население для государства {state_id} и национальности {nationality_id}")
+
+
+def populate_db():
+    for _ in range(80):  
+        state = create_state()
+        if state:
+            nationality = create_nationality()
+            if nationality:
+                create_population(state["id"], nationality["id"])
+
+if __name__ == "__main__":
+    populate_db()
